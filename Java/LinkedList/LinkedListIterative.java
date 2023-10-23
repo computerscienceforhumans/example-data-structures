70853b257f9d06bc931ce12d9f13bafe28c9d314
Reagan <7460851+ReaganDuggins@users.noreply.github.com>
2023-10-23 18:12:50 -0400

    [f] create node, restructure files for Java package structure, start Iterative Linked List

---
+++ Java/LinkedList/LinkedListIterative.java
@@ -0,0 +1,31 @@
+package Java.LinkedList;
+/*
+ * Linked List Iterative
+ * - Node
+ * -- Value
+ * -- NextNode
+ * - Linked List
+ * -- Add
+ * -- Remove
+ * -- FindAtIndex
+ * -- IndexOf
+ * -- Display
+ * -- Size
+ */
+
+public class LinkedListIterative {
+    public Node head;
+
+    public LinkedListIterative() {}
+    public LinkedListIterative(int startingValue) {
+        head = new Node(startingValue);
+    }
+
+    public void add(int addMe) {
+        /*
+         * add the first thing
+         * add the second thing
+         * add everything else
+         */
+    }
+}
